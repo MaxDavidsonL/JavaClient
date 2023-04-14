ca8e69875bb6764df2f13fc4325455fbb4973b97
MaxDavidsonL <maxime.davidson-Longpre@umontreal.ca>
2023-04-13 21:00:23 -0400

    client developpement

--- src/JavaClient.java
+++ src/JavaClient.java
@@ -1,28 +1,57 @@
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import server.models.Course;
+
+import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class JavaClient {
 
     public Socket clientSocket;
 
-    public JavaClient(String IP, int port) throws IOException {
+    public JavaClient(String IP, int port) throws IOException, ClassNotFoundException {
         this.clientSocket = new Socket(IP, port);
-        OutputStreamWriter os = new OutputStreamWriter(clientSocket.getOutputStream());
-        BufferedWriter writer = new BufferedWriter(os);
+        ObjectOutputStream os = new ObjectOutputStream(clientSocket.getOutputStream());
+        //BufferedWriter writer = new BufferedWriter(os);
+        ObjectInputStream is = new ObjectInputStream(clientSocket.getInputStream());
+
+        // Envoie au serveur les lignes tapée sur la console
         Scanner scanner = new Scanner(System.in);
-        while(scanner.hasNext()) {
+
+        String line = scanner.nextLine();
+        System.out.println("Envoi de : " + line);
+        os.writeObject(line);
+        // Vider le buffer : envoyer la requête tout de suite
+        os.flush();
+
+        ArrayList<Course> test = (ArrayList<Course>) is.readObject();
+        System.out.println(test.toString());
+        os.close();
+        is.close();
+        clientSocket.close();
+
+
+
+
+        //os.writeObject("CHARGER Hiver")
+
+
+        /*while (scanner.hasNext()) {
             String line = scanner.nextLine();
             System.out.println("Envoi de : " + line);
             writer.append(line + "\n");
             writer.flush();
-            if(line.equals("exit")) {
+            if (line.equals("exit")) {
                 System.out.println("Au revoir.");
                 break;
             }
         }
-        writer.close();
+            /*while (reader.ready()) {
+                System.out.println(reader);
+            }
+            writer.close();
+            //reader.close();
+        */
     }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        JavaClient client = new JavaClient("127.0.0.1", 1337);
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        System.out.println("*** Bienvenue au portail ***");
+        while (true) {
+            JavaClient client = new JavaClient("127.0.0.1", 1337);
+        }
     }
 }
