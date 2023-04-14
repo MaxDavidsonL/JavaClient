ea9e3e355a2b0db7bac4447d4a10f1a0d5864772
MaxDavidsonL <maxime.davidson-Longpre@umontreal.ca>
2023-04-14 19:41:16 -0400

    Implémentation de handleRegistration

--- src/JavaClient.java
+++ src/JavaClient.java
@@ -1,13 +1,16 @@
 import server.models.Course;
+import server.models.RegistrationForm;
 
 import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class JavaClient {
 
     public Socket clientSocket;
+    RegistrationForm fiche;
 
     public JavaClient(String IP, int port) throws IOException, ClassNotFoundException {
         this.clientSocket = new Socket(IP, port);
@@ -19,13 +22,47 @@ public class JavaClient {
         Scanner scanner = new Scanner(System.in);
 
         String line = scanner.nextLine();
+
+        // Prends les informations du client pour créer l'instance de registrationForm
+        if (line.equals("INSCRIRE")) {
+            System.out.println("Veuillez saisir votre prénom:  ");
+            String prenom = scanner.nextLine();
+            System.out.println("Veuillez saisir votre nom:  ");
+            String nom = scanner.nextLine();
+            System.out.println("Veuillez saisir votre email:  ");
+            String email = scanner.nextLine();
+            System.out.println("Veuillez saisir votre matricule:  ");
+            String matricule = scanner.nextLine();
+            System.out.println("Veuillez saisir le code du cours:  ");
+            String code = scanner.nextLine();
+
+            File listeCours = new File("src/data/cours.txt");
+            Scanner scan = new Scanner(listeCours);
+            Course course = null;
+            while (scan.hasNextLine()) {
+                String cours = scan.nextLine();
+                String[] ligne = cours.split("\\s");
+                String sigle = ligne[0];
+                if (Objects.equals(sigle, code)) {
+                    course = new Course(ligne[1], ligne[0], ligne[2]);
+                }
+            }
+            fiche = new RegistrationForm(prenom, nom, email, matricule, course);
+        }
         System.out.println("Envoi de : " + line);
         os.writeObject(line);
+
+        if (line.equals("INSCRIRE")) {
+            os.writeObject(fiche);
+            System.out.println(is.readObject());
+        }
         // Vider le buffer : envoyer la requête tout de suite
         os.flush();
 
-        ArrayList<Course> test = (ArrayList<Course>) is.readObject();
-        System.out.println(test.toString());
+
+
+        //ArrayList<Course> test = (ArrayList<Course>) is.readObject();
+        //System.out.println(test.toString());
         os.close();
         is.close();
         clientSocket.close();
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
         System.out.println("*** Bienvenue au portail ***");
         while (true) {
             JavaClient client = new JavaClient("127.0.0.1", 1337);
